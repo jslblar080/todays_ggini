6bb6d5f0929b5549479f21cc41215848ee55aa42
hoyoung <hoyoung1220@gmail.com>
2026-05-15 09:31:43 +0900

    식단 스타일 저장 파일

---
+++ KT_cloud_project/app/core/constants.py
@@ -0,0 +1,23 @@
+MEAL_STYLES_META = {
+    "budget_first": {
+        "style_id": "budget_first",
+        "style_name": "가성비 최우선",
+        "description": "예산을 가장 우선으로 고려한 식단",
+        "summary_comment": "예산 부담을 줄이고 간편하게 구성한 식단입니다.",
+        "source_goal": "식비 절약"
+    },
+    "easy_cooking": {
+        "style_id": "easy_cooking",
+        "style_name": "간편 조리식",
+        "description": "조리 난이도와 시간을 낮게 유지한 식단",
+        "summary_comment": "조리 부담을 줄이고 빠르게 준비할 수 있는 식단입니다.",
+        "source_goal": "간편식"
+    },
+    "high_protein":{
+        "style_id": "high_protein",
+        "style_name": "고단백 관리식",
+        "description": "단백질 섭취를 우선으로 고려한 식단",
+        "summary_comment": "단백질 섭취를 늘리고 싶은 사용자에게 적합한 식단입니다.",
+        "source_goal": "고단백"
+    }
+}
